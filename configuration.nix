d2a13ea13a9196adac9aab06b44957b7cebc1a74
Miles Breslin <milesbreslin@gmail.com>
2019-07-13 17:17:56 -0700

    Install i3 Window manager

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,27 @@ with lib;
   };
 
   boot.loader.grub.enable = false;
+  environment.pathsToLink = [ "/libexec" ];
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
+  };
+ 
 
 
   services.openssh = {
